e21f966fc1116205795f21599d773f9d486e695e
Vineet <60286768+vineetkl@users.noreply.github.com>
2020-10-18 22:20:09 +0530

    timedraw ver clock

--- app.js
+++ app.js
@@ -4,15 +4,15 @@ const hourDiv = document.getElementById('hour');
 
 setInterval(updateClock, 1000);
 
-function updateClock(){
-	let date = new Date();
-	let sec = date.getSeconds() / 60;
-	let min = (date.getMinutes() + sec) / 60;
-	let hour = (date.getHours() + min) / 12;
+function updateClock() {
+    let date = new Date();
+    let sec = date.getSeconds() / 60;
+    let min = (date.getMinutes() + sec) / 60;
+    let hour = (date.getHours() + min) / 12;
 
-	secDiv.style.transform = "rotate(" + (sec * 360) + "deg)";
-	minDiv.style.transform = "rotate(" + (min * 360) + "deg)";
-	hourDiv.style.transform = "rotate(" + (hour * 360) + "deg)";
+    secDiv.style.transform = "rotate(" + (sec * 360) + "deg)";
+    minDiv.style.transform = "rotate(" + (min * 360) + "deg)";
+    hourDiv.style.transform = "rotate(" + (hour * 360) + "deg)";
 }
 
 updateClock();
